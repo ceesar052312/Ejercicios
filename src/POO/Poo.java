19b7ad9957fdafe1bc648e7ba209d6d36c22d4df
Cesar Meza <cesarhernandezm90@gmail.com>
2026-09-09 18:49:30 -0500

    MoldeCarro

---
+++ src/POO/Poo.java
@@ -0,0 +1,46 @@
+package POO;
+
+public class Poo {
+    public class Carro {
+        public String marca;
+        public String modelo;
+        public int year;
+        public String color;
+        public int puertas;
+        public String transmision;
+        public String combustible;
+        public int velocidadMaxima;
+        public int nivelDeCombustible;
+        public int kilometraje;
+        public boolean encendido;
+        public String placa;
+        public int capacidadPasajeros;
+        public int capacidadTanque;
+        public int presionLlantas;
+
+        public Carro(String marca, String modelo, int year, String color, int puertas, String transmision, String combustible, int velocidadMaxima, int nivelDeCombustible, int kilometraje, boolean encendido, String placa, int capacidadPasajeros, int capacidadTanque, int presionLlantas) {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.year = year;
+            this.color = color;
+            this.puertas = puertas;
+            this.transmision = transmision;
+            this.combustible = combustible;
+            this.velocidadMaxima = velocidadMaxima;
+            this.nivelDeCombustible = nivelDeCombustible;
+            this.kilometraje = kilometraje;
+            this.encendido = encendido;
+            this.placa = placa;
+            this.capacidadPasajeros = capacidadPasajeros;
+            this.capacidadTanque = capacidadTanque;
+            this.presionLlantas = presionLlantas;
+        }
+    }
+
+        public static void main(String[] args) {
+
+
+
+
+        }
+}
