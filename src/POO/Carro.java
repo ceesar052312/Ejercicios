503f19b1149ec61b9ca29ee69d89745b553b7100
ceesar052312 <cesarhernandezm90@gmail.com>
2026-09-14 19:14:52 -0500

    Add files via upload
    estrellas y carros

---
+++ src/POO/Carro.java
@@ -0,0 +1,38 @@
+package POO;
+
+public class Carro {
+        public String marca;
+        public String modelo;
+        public int year;
+        public String color;
+        public int puertas;
+        public String transmision;
+        public String combustible;
+        public int velocidadMaxima;
+        public String traccion;
+        public int kilometraje;
+        public boolean encendido;
+        public String placa;
+        public int capacidadPasajeros;
+        public String tipoAsientos;
+        public String tipoLLantas;
+
+        public Carro(String marca, String modelo, int year, String color, int puertas, String transmision, String combustible, int velocidadMaxima, String traccion, int kilometraje, boolean encendido, String placa, int capacidadPasajeros, String tipoAsientos, String tipoLLantas) {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.year = year;
+            this.color = color;
+            this.puertas = puertas;
+            this.transmision = transmision;
+            this.combustible = combustible;
+            this.velocidadMaxima = velocidadMaxima;
+            this.traccion = traccion;
+            this.kilometraje = kilometraje;
+            this.encendido = encendido;
+            this.placa = placa;
+            this.capacidadPasajeros = capacidadPasajeros;
+            this.tipoAsientos = tipoAsientos;
+            this.tipoLLantas = tipoLLantas;
+
+        }
+    }
---
+++ src/POO/Estrella.java
@@ -0,0 +1,15 @@
+package POO;
+
+public class Estrella {
+    public String color;
+    public String forma;
+    public int puntas;
+    public String size;
+
+    public Estrella(String color, String forma, int puntas, String size) {
+        this.color = color;
+        this.forma = forma;
+        this.puntas = puntas;
+        this.size = size;
+    }
+}
