5b9dfc7989b3d4c09c3390fbc014108f940b5aa7
ceesar052312 <cesarhernandezm90@gmail.com>
2026-09-15 11:34:27 -0500

    Add files via upload

---
+++ src/POO/Banco/Dominio/Banco.java
@@ -0,0 +1,12 @@
+package POO.Banco.Dominio;
+
+public class Banco {
+    public String nombre;
+    public String gerente;
+
+
+    public Banco(String nombre, String gerente) {
+        this.nombre = nombre;
+        this.gerente = gerente;
+    }
+}
